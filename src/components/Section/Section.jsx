e087f1a30d7358df62f16ee848b49ff538c1fbb6
Youralexey <johndoe@example.com>
2021-09-16 21:34:05 +0300

    исправил ошибки

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import css from "./Section.module.css";
 
 export default function Section({ title, children }) {
@@ -8,3 +9,8 @@ export default function Section({ title, children }) {
     </section>
   );
 }
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired
+};
